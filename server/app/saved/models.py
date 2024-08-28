a82a3a697779859d03b11121ce95ca27a869fb16
Juan Novas <juanignacionovas@gmail.com>
2024-08-28 11:07:02 -0300

    build(models): playlist models added

--- server/app/saved/models.py
+++ server/app/saved/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import Users
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class PlaylistUser(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, validators=[MinLengthValidator(5)])
+    
+    
+class PlaylistSongs(models.Model):
+    playlist = models.ForeignKey(PlaylistUser, on_delete=models.CASCADE)
+    song_id = models.PositiveIntegerField()
+    order  = models.PositiveSmallIntegerField()
