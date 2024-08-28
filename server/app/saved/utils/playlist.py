d191cc451dbde5d6f45f04c0c9c3937ef0871954
Juan Novas <juanignacionovas@gmail.com>
2024-08-28 17:35:38 -0300

    feat(api): playlist info now return its songs

--- server/app/saved/utils/playlist.py
+++ server/app/saved/utils/playlist.py
@@ -1,8 +1,14 @@
-from saved.models import PlaylistUser
+from saved.models import PlaylistUser, PlaylistSongs
 
 def generate_info_dict(playlist: PlaylistUser) -> dict:
+    songs = PlaylistSongs.objects.filter(playlist=playlist).order_by('order')
+    songs_data = []
+    for song in songs:
+        songs_data.append(song.song_id)
     playlist_data = {
         "name": playlist.name,
-        "id": playlist.pk
+        "songs": playlist.song_count,
+        "id": playlist.pk,
+        "songs": songs_data
     }
     return playlist_data
