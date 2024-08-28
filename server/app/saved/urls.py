1547d17e21c3f0c11fbee8fe6f8fd52ce238f836
Juan Novas <juanignacionovas@gmail.com>
2024-08-28 16:42:30 -0300

    feat(api): update playlist

--- server/app/saved/urls.py
+++ server/app/saved/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('playlist/create', CreatePlaylist.as_view(), name='create_playlist'),
-    path('playlist/get/', GetPlaylist.as_view(), name='create_playlist'),
-    path('playlist/get/<int:playlist_id>', GetPlaylist.as_view(), name='create_playlist'),
+    path('playlist/get/', GetPlaylist.as_view(), name='get_playlists'),
+    path('playlist/get/<int:playlist_id>', GetPlaylist.as_view(), name='get_playlist_by_id'),
+    path('playlist/update/<int:playlist_id>', UpdatePlaylistProperties.as_view(), name='update_playlist'),
 ]
--- server/app/saved/views.py
+++ server/app/saved/views.py
@@ -56,4 +56,42 @@ class GetPlaylist(View):
             })
         
         return JsonResponse({'playlists': playlists_data}, status=200)
+    
+    
+@method_decorator(csrf_exempt, name="dispatch")
+class UpdatePlaylistProperties(View):
+    
+    @jwt_required
+    def patch(self, request, *args, **kwargs):
+        playlist_id = kwargs.get('playlist_id', None)
+        
+        # Gets the playlist
+        playlist = PlaylistUser.objects.filter(user=request.user, pk=playlist_id).first()
+        if not playlist:
+            return JsonResponse({'error': "playlist not found"}, status=404)
+        
+        # Checks if there is a json
+        try:
+            body = json.loads(request.body)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'error': 'JSON required'}, status=400)
+        
+        # Applies the changes
+        if name := body.get("name"):
+            playlist.name = name
             
+        # Saves the changes
+        try:
+            playlist.full_clean()
+            playlist.save()
+        except ValidationError as e:
+            return JsonResponse({'error': e.message_dict}, status=400)
+        
+        # Formats the data to return
+        playlists_data = {
+            "name": playlist.name,
+            "id": playlist.pk
+        }
+        
+        return JsonResponse({'status': 'succes',"playlist":playlists_data}, status=200)
+        
