3efb1287a16fff0d573e39f217c6138e734e7266
Juan Novas <juanignacionovas@gmail.com>
2024-08-29 19:14:15 -0300

    feat(playlist): data response contains favorite field

--- server/app/saved/utils/playlist.py
+++ server/app/saved/utils/playlist.py
@@ -1,13 +1,17 @@
-from saved.models import PlaylistUser, PlaylistSongs
+from saved.models import PlaylistUser, PlaylistSongs, Favorite
 
 def generate_info_dict(playlist: PlaylistUser) -> dict:
     songs = PlaylistSongs.objects.filter(playlist=playlist).order_by('order')
+    favorites = set(Favorite.objects.filter(user=playlist.user).values_list('element_id', flat=True))
     songs_data = []
     for song in songs:
-        songs_data.append(song.song_id)
+        songs_data.append({
+            "id":song.song_id,
+            "favorite":song.song_id in favorites
+        })
     playlist_data = {
         "name": playlist.name,
-        "songs": playlist.song_count,
+        "songs amount": playlist.song_count,
         "id": playlist.pk,
         "songs": songs_data
     }
