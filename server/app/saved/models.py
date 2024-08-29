2b1326ca1e2bd01aeb985a83eba0b3c9937db6d2
Juan Novas <juanignacionovas@gmail.com>
2024-08-29 15:57:30 -0300

    build(models): favourites model added

--- server/app/saved/models.py
+++ server/app/saved/models.py
@@ -19,3 +19,18 @@ class PlaylistSongs(models.Model):
 
     def __str__(self):
         return str(self.song_id)
+    
+    
+class Favourite(models.Model):
+    OPTIONS = [
+        (1, 'song'),
+        (2, 'album'),
+        (3, 'singuer'),
+    ]
+    
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    element_id = models.PositiveIntegerField()
+    category = models.SmallIntegerField(choices=OPTIONS)
+    
+    def __str__(self):
+        return f"{self.get_category_display()} #{self.element_id}"
