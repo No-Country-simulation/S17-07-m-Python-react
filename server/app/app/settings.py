ea9ef1c259501ae472bf01e271bc2af5995b8178
Juan Novas <juanignacionovas@gmail.com>
2024-09-09 16:56:54 -0300

    build(settings): radio app added to apps

--- server/app/app/settings.py
+++ server/app/app/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    "radio",
     "saved",
     "search",
+    "radio",
     "users",
     "history",
 ]
