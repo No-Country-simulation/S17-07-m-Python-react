ae69770dfb937f3fde4caf986a98d5975e45ffd9
Juan Novas <juanignacionovas@gmail.com>
2024-09-12 09:28:28 -0300

    build(discover): app created

--- server/app/app/settings.py
+++ server/app/app/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "radio",
     "users",
     "history",
+    "discover",
 ]
 
 MIDDLEWARE = [
--- server/app/app/urls.py
+++ server/app/app/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("api/", include("history.urls")),
     path("api/", include("search.urls")),
     path("api/", include("radio.urls")),
+    path("api/", include("discover.urls")),
 ]
